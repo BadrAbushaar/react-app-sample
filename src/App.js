91f9edd37c4e629d994879f18f133bf8eb1119e7
Irrom <31070777+Bytestorm5@users.noreply.github.com>
2024-04-27 16:26:29 -0400

    Bar Chart!
    Needs callback for radio buttons, will do in a bit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
-import TIPS from './tips.csv'
+import TIPS from './tips.csv';
 import './App.css';
 import BarChart from "./BarChart";
 import CorrMatrix from "./CorrMatrix";
@@ -8,45 +8,57 @@ import Scatter from "./Scatter";
 
 class App extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             columns: [],
-            selected_column: 'total_bill'
-        }
+            data: null, // Add 'data' to the initial state
+            selected_column: "total_bill"
+        };
     }
+
     componentDidMount() {
-        let data = d3.csv(TIPS).then((data) => {
-            this.setState({'columns': Object.keys(data[0])})
-            //this.setState({'selected_column': this.state.columns[0]})
-        })
+        d3.csv(TIPS).then((data) => {
+            const columns = Object.keys(data[0]);
+            this.setState({
+                columns: columns,
+                data: data // Set the data in the state
+            });
+        });
     }
-    componentDidUpdate() {
-        console.log(this.state)
-    }
+
     render() {
+        const { columns, data, selected_column } = this.state;
+
+        if (!data) {
+            return <div>Loading data...</div>; // Handle the loading state
+        }
+
         return (
             <div>
                 <div id="header">
                     <span>Select Target:</span>
-                    <select id='col_selector' onChange={(event) => {this.setState({selected_column: event.target.value})}}>
-                        {this.state.columns.map((col, idx) => (
-                            <option key={col}>{col}</option>
+                    <select id="col_selector" onChange={(e) => this.setState({ selected_column: e.target.value })}>
+                        {columns.map((col) => (
+                            <option key={col} value={col}>
+                                {col}
+                            </option>
                         ))}
                     </select>
                 </div>
-                <div class="graph-container">
+                <div className="graph-container">
                     <div id="bar-chart">
-                        <BarChart></BarChart>
+                        <BarChart data={data} selected_column={selected_column} />
                     </div>
                     <div id="corr-matrix">
-                        <CorrMatrix></CorrMatrix>
+                        <CorrMatrix data={data} />
                     </div>
                     <div id="scatter">
-                        <Scatter></Scatter>
+                        <Scatter data={data} selected_column={selected_column} />
                     </div>
-                </div> 
+                </div>
             </div>
-        )
+        );
     }
 }
-export default App
+
+export default App;
